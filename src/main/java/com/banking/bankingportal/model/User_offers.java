2d994d6c0b18d3a0717ddcb4805907546d092eeb
Kanan Guleria <kanan.guleria@happiestminds.com>
2022-12-09 15:07:27 +0530

    user offets table and jpa

---
+++ src/main/java/com/banking/bankingportal/model/User_offers.java
@@ -0,0 +1,67 @@
+package com.banking.bankingportal.model;
+
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class User_offers {
+	
+	@Id
+	private int offer_id;
+	
+	private String offer_desc;
+	
+	private Date start_date;
+	private Date expiry_date;
+	
+	@ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+	public int getOffer_id() {
+		return offer_id;
+	}
+
+	public void setOffer_id(int offer_id) {
+		this.offer_id = offer_id;
+	}
+
+	public String getOffer_desc() {
+		return offer_desc;
+	}
+
+	public void setOffer_desc(String offer_desc) {
+		this.offer_desc = offer_desc;
+	}
+
+	public Date getStart_date() {
+		return start_date;
+	}
+
+	public void setStart_date(Date start_date) {
+		this.start_date = start_date;
+	}
+
+	public Date getExpiry_date() {
+		return expiry_date;
+	}
+
+	public void setExpiry_date(Date expiry_date) {
+		this.expiry_date = expiry_date;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/banking/bankingportal/repo/UserOffersRepo.java
@@ -0,0 +1,9 @@
+package com.banking.bankingportal.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.banking.bankingportal.model.User_offers;
+
+public interface UserOffersRepo extends JpaRepository<User_offers,Integer>{
+
+}
