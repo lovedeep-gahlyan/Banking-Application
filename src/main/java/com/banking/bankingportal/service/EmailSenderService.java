11829140912f917fe7d99f79ca28d0d82a52172d
Abhinav Chaudhary <abhinav.chaudhary@happiestminds.com>
2022-12-30 10:42:32 +0530

    added email udpate for transaction

--- src/main/java/com/banking/bankingportal/service/EmailSenderService.java
+++ src/main/java/com/banking/bankingportal/service/EmailSenderService.java
@@ -13,7 +13,7 @@ public class EmailSenderService {
 	
 	public void sendEmail(String toEmail,String subject,String body) {
 		SimpleMailMessage message = new SimpleMailMessage();
-		message.setFrom("lovedeepgahlyan1999@gmail.com");
+		message.setFrom("abhihappiest10@gmail.com");
 		message.setTo(toEmail);
 		message.setText(body);
 		message.setSubject(subject);
